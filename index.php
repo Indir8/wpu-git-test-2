b1be81ec872e8bc1306a1930415bb11f86e8aa9e
Indir8 <nadiaipr05@gmail.com>
2024-08-09 12:03:03 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ipsa ut, voluptatum, explicabo labore, culpa est a assumenda rem eos voluptates fuga sapiente iusto. Optio dolor sit ipsam fuga quidem!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
